717a71987fbe2faf0ae8fba9462e5a4301cce98b
Ramses Solano Ramirez <ramsessr@outlook.com>
2025-12-08 16:19:56 -0500

    first commit

---
+++ src/main/java/com/sena/rsr/emk/financial_planning_service/FinancialPlanningServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sena.rsr.emk.financial_planning_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialPlanningServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialPlanningServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sena/rsr/emk/financial_planning_service/FinancialPlanningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.rsr.emk.financial_planning_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialPlanningServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
